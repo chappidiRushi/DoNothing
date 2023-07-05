865bce41d70660c540d402497160ec72ca702a13
chappidiRushi <chappidi.rushi@gmail.com>
2023-07-05 16:10:02 +0530

    Added new component

--- app/src/main/java/com/rushi/donothing/MainActivity.kt
+++ app/src/main/java/com/rushi/donothing/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.rushi.donothing
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
@@ -10,21 +13,31 @@ import android.widget.Toast
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main);
-        val textView = findViewById<TextView>(R.id.mainText);
-        val textInput = findViewById<EditText>(R.id.mainInput);
-        val btn = findViewById<Button>(R.id.mainButton);
+        setContentView(R.layout.activity_main)
+        val textView = findViewById<TextView>(R.id.mainText)
+        val textInput = findViewById<EditText>(R.id.mainInput)
+        val btn = findViewById<Button>(R.id.mainButton)
+        val nextBtn = findViewById<Button>(R.id.nextBtn)
+        var updatedName = ""
         btn.setOnClickListener {
-            val updatedName = textInput.text.toString();
+            updatedName = textInput.text.toString()
             if (updatedName ==""){
-                Toast.makeText(this@MainActivity, "Please Enter a Name", Toast.LENGTH_LONG).show()
-
+                Toast.makeText(this@MainActivity, "Please Enter a Name",
+                    Toast.LENGTH_LONG).show()
+                nextBtn.visibility = INVISIBLE
             }else{
-                val message = "Hello $updatedName";
-                textView.text = message;
-                textInput.text.clear();
+                updatedName = "Hello $updatedName"
+                textView.text = updatedName
+                textInput.text.clear()
+                nextBtn.visibility = VISIBLE
+            }
+            }
+        nextBtn.setOnClickListener{
+            val nextIntent = Intent(this, SecondActivity::class.java)
+//            val updatedName = textInput.text.toString()
 
-            }
-            }
+            nextIntent.putExtra("USER",updatedName)
+            startActivity(nextIntent)
+        }
     }
 }
