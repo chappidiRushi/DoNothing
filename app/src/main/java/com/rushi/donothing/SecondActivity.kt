55f8be8fddd13abf2f4ee6911ae394d5a14d7fd9
chappidiRushi <chappidi.rushi@gmail.com>
2023-07-05 16:10:54 +0530

    second activity

---
+++ app/src/main/java/com/rushi/donothing/SecondActivity.kt
@@ -0,0 +1,16 @@
+package com.rushi.donothing
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+        val userName = intent.getStringExtra("USER")
+        val textView = findViewById<TextView>(R.id.SecondText)
+        textView.text = "welcome $userName"
+
+    }
+}
